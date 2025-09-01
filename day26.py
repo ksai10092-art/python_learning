b4f450ad567e6dbe46366b9ba96615e4732b82ad
krishna sai <ksai10092@gmail.com>
2025-09-01 10:11:15 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,39 @@
+#def approch():
+##   y=int(input("enter any number: "))
+  #  z=x+y
+   # v=x-y
+    #f=x*y
+#    print(f" the sum is:{z}")
+     print(f"the sub is :{v}")
+     print(f"the muliple:{f}")
+approch()
+def sai():
+
+    a=int(input("enter first value:"))
+    b=int(input("enter second value:"))
+    c=a+b
+    d=a-b
+    e=a*b
+    if b==0:
+       print("division by zeronot possible ")
+    else:
+        f=a/b
+    #process
+    #output
+    print("sum of ({},{}) is ={}".format(a,b,c))
+    print("sub of ({},{}) is ={}".format(a,b,d))
+    print("muliple of ({},{}) is ={}".format(a,b,e))
+    print("div of ({},{}) is ={}".format(a,b,f))
+#sai()
+#approch
+def approch():
+    a=float(input("enter first value:"))
+    b=float(input("enter secound value:"))
+    d=a+b
+    e=a*b
+    f=a/b
+    if b===0
+    return(f"the sum is {d} \n" f" the multiple is {e}\n" f"the div is {f} \n")
+r=approch
+print(r)
+
